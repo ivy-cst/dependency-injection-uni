aa699e991a960eb3f17e8be896801e5dec539de5
Christian Strebel <christian.strebel@axonivy.com>
2024-01-24 10:42:02 +0100

    Add guice and instance Manager with it

--- dependency-injection-uni/src/test/java/ch/ivyteam/di/uni/TestServices.java
+++ dependency-injection-uni/src/test/java/ch/ivyteam/di/uni/TestServices.java
@@ -4,15 +4,20 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
 import ch.ivyteam.di.uni.impl.NiceServiceImpl;
 import ch.ivyteam.di.uni.impl.OtherServiceImpl;
 import ch.ivyteam.di.uni.impl.SuperManager;
 
 class TestServices {
 
+  private final Injector injector = Guice.createInjector();
+
   @Test
   void niceService() {
-    Manager superManager = new SuperManager();
+    Manager superManager = injector.getInstance(SuperManager.class);
     NiceService niceService = new NiceServiceImpl(superManager);
     assertEquals("Nice", niceService.name());
     assertEquals("Super", niceService.manager().name());
@@ -21,10 +26,11 @@ class TestServices {
 
   @Test
   void otherService() {
-    Manager superManager = new SuperManager();
+    Manager superManager = injector.getInstance(SuperManager.class);
     OtherService niceService = new OtherServiceImpl(superManager);
     assertEquals("Other", niceService.name());
     assertEquals("Super", niceService.manager().name());
     assertEquals("OtherServiceImpl:Other of SuperManager", niceService.info());
   }
+
 }
